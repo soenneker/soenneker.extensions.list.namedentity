ede84b2827f691e0e18bfdec98d981a456b6694b
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:02:00 -0500

    TUnit conversion

--- test/Soenneker.Extensions.List.NamedEntity.Tests/ListNamedEntityExtensionTests.cs
+++ test/Soenneker.Extensions.List.NamedEntity.Tests/ListNamedEntityExtensionTests.cs
@@ -1,11 +1,10 @@
 using Soenneker.Tests.Unit;
-using Xunit;
 
 namespace Soenneker.Extensions.List.NamedEntity.Tests;
 
 public class ListNamedEntityExtensionTests : UnitTest
 {
-    [Fact]
+    [Test]
     public void Default()
     {
 
